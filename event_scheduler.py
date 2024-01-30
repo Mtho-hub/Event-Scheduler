00ff46d60b9205867715d6b5cae6a77d8e024682
Mtho-hub <85952814+Mtho-hub@users.noreply.github.com>
2024-01-30 23:03:59 +0200

    Add files via upload

---
+++ event_scheduler.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 16:21:20 2024
+
+@author: mtho
+"""
+
+from datetime import date
+
+
+''' Classes, Constants and Global Variables'''
+#------------------------------------------------------------------------------
+class Event:
+    def __init__(self, title, description, date, hour, minute):
+        self.title = title
+        self.description = description
+        self.date = date
+        self.hour = hour
+        self.minute = minute
+        
+    def __str__(self):
+        if self.hour < 10: hour = '0' + str(self.hour)
+        else: hour = str(self.hour)
+        if self.minute < 10: minute = '0' + str(self.minute)
+        else: minute = str(self.minute)
+        return 'Title: {}\nDescription: {}\nDate: {}\nTime: {}:{}'.format(self.title, self.description,
+                                                                       self.date, hour, minute)
+
+
+base_view_text = '\nWelcome to Event Scheduler Application (TM).\nPlease select'\
+                 ' an option below by entering the corresponding number:\n'\
+                 '1. View Events\n2. Create Event\n3. Delete Event\n4. Exit'
+                 
+events = []
+#------------------------------------------------------------------------------
+        
+
+''' Functions'''
+#------------------------------------------------------------------------------
+def string_to_date(date_string):
+    if len(date_string) != 10:
+        print('Invalid Date.')
+        return None
+    
+    try:
+        if date_string[4] != '-' or date_string[7] != '-':
+            print('Invalid Date.')
+            return None
+        
+        year = int(date_string[0:4])
+        month = int(date_string[5:7]) 
+        day = int(date_string[8:10])
+        
+        event_date = date(year, month, day)
+        return event_date
+    except:
+        print('Invalid Date.')
+        return None
+    
+    
+def valid_date(event_date):       
+    if event_date < date.today():
+        print('Invalid Date. Cannot create events in the past.')
+        return False
+    return True
+
+
+def string_to_time(time_string):
+    if len(time_string) != 5:
+        print('Invalid Time.')
+        return None
+    
+    try:
+        if time_string[2] != ':':
+            print('Invalid Time.')
+            return None
+        
+        hour = int(time_string[0:2])
+        minute = int(time_string[3:5]) 
+        
+        if hour < 0 or hour > 24:
+            print('Invalid Time.')
+            return None
+        if minute < 0 or minute > 60:
+            print('Invalid Time.')
+            return None
+        
+        return hour, minute
+    except:
+        print('Invalid Time.')
+        return None
+
+
+def create_event(title, description, date, time):
+    event = Event(title, description, date, time[0], time[1])
+    events.append(event)
+    events.sort(key=lambda x: (x.date, x.hour, x.minute))
+    print('\nEvent created successfully!')
+#------------------------------------------------------------------------------              
+
+
+while True:
+    print(base_view_text)
+    command = input('>>> ')
+    
+    
+    # 1. View Events
+    if command == '1':
+        for i in range(len(events)):
+            print('\n' + str(i+1) + '. ' + str(events[i]) + '\n')
+            
+    
+    # 2. Create Event
+    if command == '2':
+        cancelled = False
+        
+        # Get title
+        while cancelled != True:
+            print('\nPlease enter the event title: (0 to cancel)')
+            title = input('>>> ')
+            
+            if title == '0': cancelled = True
+            elif title != '': break 
+        
+        # Get description
+        while cancelled != True:
+            print('\nPlease enter the event description: (0 to cancel)')
+            description = input('>>> ')
+            
+            if description == '0': cancelled = True
+            elif description != '': break 
+            
+        # Get date
+        while cancelled != True:
+            print('\nPlease enter the event date in the format \'YYYY-MM-DD\': (0 to cancel)')
+            event_date = input('>>> ')
+            
+            if event_date == '0': 
+                cancelled = True
+            else:
+                event_date = string_to_date(event_date)
+                if event_date != None and valid_date(event_date): break
+            
+        # Get time
+        while cancelled != True:
+            print('\nPlease enter the event time in the format \'HH:MM\': (0 to cancel)')
+            event_time = input('>>> ')
+            
+            if event_time == '0': 
+                cancelled = True
+            else:
+                event_time = string_to_time(event_time)
+                if event_time != None: break
+        
+        if cancelled == False:
+            create_event(title, description, event_date, event_time)
+            
+    
+    # 3. Delete Event
+    if command == '3':
+        for i in range(len(events)):
+            print('\n' + str(i+1) + '. ' + str(events[i]) + '\n')
+            
+        while True: 
+            print('\nPlease select an event to delete by entering the corresponding title: (0 to cancel)')
+            title_to_del = input('>>> ')
+            
+            if title_to_del == '0': break
+        
+            event_deleted = False
+
+            for event in events:
+                # case sensitive
+                if event.title == title_to_del:
+                    events.remove(event)    
+                    print('\nEvent deleted successfully!')
+                    event_deleted = True
+                    break
+                
+            if event_deleted == False:
+                print('Event cannot be found.')
+                continue
+            
+            break
+            
+                  
+    # 4. Exit
+    if command == '4':
+        break
+            
